320cb7b419dd32934f72ae18bd7e3ab0cf555146
Luiz Victor (Smith) <luiz.victor100@hotmail.com>
2021-03-13 23:06:35 -0300

    Second Complete Challenge

--- src/index.js
+++ src/index.js
@@ -10,19 +10,88 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(
+      user => user.username === username
+  );
+
+  if (!user) {
+      return response.status(404).json({
+          error: "User not found"
+      });
+  }
+
+  request.user = user
+  return next();
 }
 
-function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+function checksCreateTodosUserAvailability(request, response, next) 
+{
+  const { user } = request;
+
+  if (user.pro || user.todos.length < 10) {
+    return next();
+  }
+
+  return response.status(403).json({
+    error: "limit of the community contract"
+  })
+  
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const { username } = request.headers;
+
+  const user = users.find(
+      user => user.username === username
+  );
+
+  if (!user) {
+      return response.status(404).json({
+          error: "User not found"
+      });
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({
+      error: "todo not found"
+    })
+  }
+
+  const todo = user.todos.find(
+    todo => todo.id === id
+  )
+
+  if (!todo) {
+    return response.status(404).json({
+      error: "todo not found"
+    })
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const user = users.find(
+    user => user.id === id
+  )
+
+  if (!user) {
+    return response.status(404).json({
+        error: "User not found"
+    });
+  }
+
+  request.user = user;
+
+  next();
 }
 
 app.post('/users', (request, response) => {
